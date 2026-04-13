cd9b1f4ddba65f6c8aa815f46f522533d28a26c7
ViniciusHezin <viniciuscarvalhohezin@hotmail.com>
2026-04-13 16:49:56 +0100

    safely storing and organizing the actual data

--- apps/teams/admin.py
+++ apps/teams/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Organisation, Department, TeamType, Team, TeamDependency
 
-# Register your models here.
+admin.site.register(Organisation)
+admin.site.register(Department)
+admin.site.register(TeamType)
+admin.site.register(Team)
+admin.site.register(TeamDependency)
