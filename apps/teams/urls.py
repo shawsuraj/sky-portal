0aa48728468f1bf84d7faadd576f0d07fcb17e7b
ViniciusHezin <viniciuscarvalhohezin@hotmail.com>
2026-04-14 11:36:39 +0100

    Organization, team directory, departments and team type features

--- apps/teams/urls.py
+++ apps/teams/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'teams' # This fixes the "namespace" error!
+app_name = 'teams' 
 
 urlpatterns = [
-    # This says: when someone goes to /teams/, run the 'teams_list' view
     path('', views.views_list, name='teams_list'), 
-]
-
-app_name = 'teams'
-urlpatterns = [
-    path('', views.views_list, name='teams_list'),
-    path('departments/', views.department_list, name='department_list'),
+    path('departments/', views.department_list, name='department_list'), 
+    path('organisations/', views.organisation_list, name='organisation_list'), 
+    path('team-types/', views.team_type_list, name='team_type_list'),
 ]
--- apps/teams/views.py
+++ apps/teams/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
 from apps.teams.models import Department
+from apps.teams.models import Organisation
+from apps.teams.models import TeamType
+from apps.teams.models import Team
 
 def views_list(request):
-    return render(request, 'teams/teams.html')
+    teams = Team.objects.select_related('department', 'team_type', 'manager_user') \
+            .prefetch_related(
+                'upstream_dependencies__upstream_team',
+                'downstream_dependencies__downstream_team'
+            ).all()
+    context = {
+        'teams': teams,
+    }
+    return render(request, 'teams/teams.html', context)
 
 def department_list(request):
 
@@ -14,3 +25,23 @@ def department_list(request):
     }
 
     return render(request, 'teams/departments.html', context)
+
+def organisation_list(request):
+    organisations = Organisation.objects.prefetch_related('departments').all()
+
+    context = {
+        'organisations': organisations,
+    }
+
+    return render(request, 'teams/organisations.html', context)
+
+def team_type_list(request):
+    # Grabs all the team categories from the database
+    team_types = TeamType.objects.all()
+
+    context = {
+        'team_types': team_types,
+    }
+
+    return render(request, 'teams/team_types.html', context)
+
