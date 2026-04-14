33aaa63033fa429ce5a506d0ecf97641cc63d645
Suraj Shaw <surajshaw1711@gmail.com>
2026-04-14 12:21:09 +0100

    Revert "Feature/teams"

--- apps/teams/admin.py
+++ apps/teams/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Organisation, Department, TeamType, Team, TeamDependency
 
-admin.site.register(Organisation)
-admin.site.register(Department)
-admin.site.register(TeamType)
-admin.site.register(Team)
-admin.site.register(TeamDependency)
+# Register your models here.
--- apps/teams/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'teams' # This fixes the "namespace" error!
-
-urlpatterns = [
-    # This says: when someone goes to /teams/, run the 'teams_list' view
-    path('', views.views_list, name='teams_list'), 
-]
