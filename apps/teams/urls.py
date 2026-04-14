0de0409ce82f6919d2c7921860f9835b98ccf350
ViniciusHezin <viniciuscarvalhohezin@hotmail.com>
2026-04-14 10:35:01 +0100

    Departments View

--- apps/teams/urls.py
+++ apps/teams/urls.py
@@ -7,3 +7,9 @@ urlpatterns = [
     # This says: when someone goes to /teams/, run the 'teams_list' view
     path('', views.views_list, name='teams_list'), 
 ]
+
+app_name = 'teams'
+urlpatterns = [
+    path('', views.views_list, name='teams_list'),
+    path('departments/', views.department_list, name='department_list'),
+]
--- apps/teams/views.py
+++ apps/teams/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
+from apps.teams.models import Department
+
 def views_list(request):
     return render(request, 'teams/teams.html')
+
+def department_list(request):
+
+    departments = Department.objects.select_related('organisation', 'dept_leader_user').all()
+
+    context = {
+        'departments': departments,
+    }
+
+    return render(request, 'teams/departments.html', context)
