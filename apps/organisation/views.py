e8beceaf1c6f970ffae02d515ec71139f3ee368f
w1973474 <w1973474@westminster.ac.uk>
2026-04-24 20:02:39 +0100

    Update views.py
    Add a view to display the page with the report integrated.

--- apps/organisation/views.py
+++ apps/organisation/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Report
+
 # Create your views here.
+
+def org_report(request):
+    return render(request, 'organisation/org_report.html', {
+        'organisation': Report.objects.all(),
+    })
