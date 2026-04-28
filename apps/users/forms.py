2090f9520597e32e6247cc561559fc4e40be2c8a
hamdankz <55181431+hamdankz@users.noreply.github.com>
2026-04-28 09:45:26 +0100

    User fixes

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -11,7 +11,7 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("forename", "surname", "username", "email", "phone", "password1", "password2")
+        fields = ("username", "email", "password1", "password2")
 
 
 class ProfileForm(forms.ModelForm): # Extra details for the user
